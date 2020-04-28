d6cecbf93eb980b5d60967a7cb39bad1fd4cd694
jeremynathan <jeremy.nathan1@gmail.com>
2020-04-29 03:15:42 +0800

    Test

--- listofvenues.php
+++ listofvenues.php
@@ -1,6 +1,6 @@
 <?php
 
-ini_set('display_errors', 1);
+// ini_set('display_errors', 1);
 
 // Starts the session to store data in global variables.
 // These global variables can be called in other pages too. 
