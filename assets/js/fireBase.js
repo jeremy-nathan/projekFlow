265c4ef10c85ee87f88467c31c715bb5f3cc65fa
jeremynathan <jeremy.nathan1@gmail.com>
2020-04-30 14:25:19 +0800

    remove credentials

--- assets/js/fireBase.js
+++ assets/js/fireBase.js
@@ -2,14 +2,14 @@ var app_firebase = {};
 (function () {
     // Your web app's Firebase configuration
     var firebaseConfig = {
-        apiKey: "AIzaSyATa9yGV000iVrO-_d-z2fToEHWNEC7r10",
-        authDomain: "fir-php-4bdd0.firebaseapp.com",
-        databaseURL: "https://fir-php-4bdd0.firebaseio.com",
-        projectId: "fir-php-4bdd0",
-        storageBucket: "fir-php-4bdd0.appspot.com",
-        messagingSenderId: "235554520478",
-        appId: "1:235554520478:web:fa027bd1f5a0d59cfd6a8f",
-        measurementId: "G-3GGPTN9XKD"
+        apiKey: "#####################",
+        authDomain: "##########################",
+        databaseURL: "################################",
+        projectId: "#####################",
+        storageBucket: "#####################",
+        messagingSenderId: "#####################",
+        appId: "#####################",
+        measurementId: "#####################"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
